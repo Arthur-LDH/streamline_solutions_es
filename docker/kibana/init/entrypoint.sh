3bef3f297ff74d3cf94046ba33c201990e1a704a
Arthur <arthurldh@gmail.com>
2024-12-10 10:25:40 +0100

    first commit

--- docker/kibana/init/entrypoint.sh
+++ docker/kibana/init/entrypoint.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+set -e
+
+# Démarrer Kibana
 /usr/share/kibana/bin/kibana &
-python3 /usr/local/bin/init_kibana.py
+
+## Attendre que Kibana soit prêt
+#until curl -s http://localhost:5601/api/status | grep -q '"overall":{"level":"available"'
+#do
+#    echo "Waiting for Kibana..."
+#    sleep 10
+#done
+#
+## Initialiser Kibana
+#python3 /usr/local/bin/init_kibana.py
+
+# Maintenir le conteneur actif
 wait
--- docker/kibana/init/init_kibana.py
+++ docker/kibana/init/init_kibana.py
@@ -6,14 +6,19 @@ import os
 KIBANA_URL = "http://kibana:5601"
 HEADERS = {"kbn-xsrf": "true", "Content-Type": "application/json"}
 
-def wait_for_kibana():
-    while True:
-        try:
-            if requests.get(f"{KIBANA_URL}/api/status").status_code == 200:
-                return
-        except:
-            time.sleep(10)
+# def wait_for_kibana(timeout=300):  # 5 minutes timeout
+#     start_time = time.time()
+#     while time.time() - start_time < timeout:
+#         try:
+#             response = requests.get(f"{KIBANA_URL}/api/status")
+#             if response.status_code == 200:
+#                 return True
+#         except requests.exceptions.RequestException:
+#             print("Waiting for Kibana to be ready...")
+#         time.sleep(10)
+#     raise TimeoutError("Kibana did not become available within the timeout period")
 
+# Créée les data views pour voir si nos données sont bien chargées
 def create_data_views():
     data_views_dir = "/usr/share/kibana/init/data_views"
     for filename in os.listdir(data_views_dir):
@@ -28,6 +33,9 @@ def create_data_views():
                 print(f"Data view {filename}: Status {response.status_code}")
                 print(f"Response: {response.text}")
 
+
+# Todo: Créer les dashboards
+# Le json ne fonctionne pas
 def create_dashboards():
     dashboards_dir = "/usr/share/kibana/init/dashboards"
     for filename in os.listdir(dashboards_dir):
@@ -43,6 +51,6 @@ def create_dashboards():
                 print(f"Response: {response.text}")
 
 if __name__ == "__main__":
-    wait_for_kibana()
+    # wait_for_kibana()
     create_data_views()
     create_dashboards()
